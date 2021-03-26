938d52524f75ba09801d6d08137e1d844c9f3614
82106 <82106@DESKTOP-2D8OQ7G>
2021-03-26 17:00:42 +0900

    array style

--- src/JavaCalendar/Calendar.java
+++ src/JavaCalendar/Calendar.java
@@ -3,7 +3,13 @@ package JavaCalendar;
 import java.util.Scanner;
 
 public class Calendar {
-
+	private final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int maxDaysOfMonth (int month) {
+		return MAX_DAYS[month-1];
+		
+	}
+		
 	public static void main (String[] args) {
 //		System.out.println(" 일 월  화 수  목  금 토");
 //		System.out.println("----------------------");
@@ -12,26 +18,16 @@ public class Calendar {
 //		System.out.println(" 15 16 17 18 19 20 21");
 //		System.out.println(" 22 23 24 25 26 27 28");
 		
-		System.out.println("달을 입력하세요");
+		System.out.println("월을 입력하세요");
 		Scanner scanner = new Scanner(System.in);
-		int monthNum = scanner.nextInt();
+		Calendar cal = new Calendar();
+		int month = scanner.nextInt();
+		System.out.println(month+"월은"+cal.maxDaysOfMonth(month)+"일까지 있습니다");
+		
+		
 		scanner.close();
 		
-		if(monthNum == 2) {
-			System.out.println("2월은 28일까지 있습니다");
-		}
-		else if (monthNum == 8) {
-			System.out.println("8월은 31일까지 있습니다");
-		}
-		else if(monthNum % 2 == 0) {//짝수 달이면 30일,2월,8월제외
-			System.out.println(monthNum + "월은 30일 까지 있습니다");
-		}
-		else if (monthNum % 2== 1) {//홀수 달이면 31일
-			System.out.println(monthNum + "월은 31일 까지 있습니다");
-		}
-		else if(monthNum < 1 || monthNum >12){//1~12 월만 입력가능하게
-			System.out.println("1년은 1월부터 12월까지 있습니다");
-		}
+		
 		
 	}
 }
